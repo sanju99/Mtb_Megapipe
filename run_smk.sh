a61eb4f891d28ef9157ed110104746c6bc0ed618
Sanjana Kulkarni <32857277+sanju99@users.noreply.github.com>
2026-06-11 12:43:57 -0400

    Update run_smk.sh

--- run_smk.sh
+++ run_smk.sh
@@ -1,12 +1,4 @@
 #!/bin/bash 
-#SBATCH -c 8
-#SBATCH -t 0-11:59
-#SBATCH -p short
-#SBATCH --mem=50G
-#SBATCH -o /home/sak0914/Errors/zerrors_%j.out 
-#SBATCH -e /home/sak0914/Errors/zerrors_%j.err
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=skulkarni@g.harvard.edu
 
 source activate snakemake
 
