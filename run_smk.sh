faacd6c96d1ca4c6da909c242c24c2d3145fb67e
Sanjana Kulkarni <32857277+sanju99@users.noreply.github.com>
2026-06-11 12:40:33 -0400

    Update run_smk.sh

--- run_smk.sh
+++ run_smk.sh
@@ -10,18 +10,16 @@
 
 source activate snakemake
 
-snakemake --snakefile /home/sak0914/Mtb_Megapipe/snakefile \
-          --use-conda --conda-frontend conda --conda-prefix /home/sak0914/Mtb_Megapipe/.snakemake/conda \
+snakemake --snakefile snakefile \
+          --use-conda --conda-frontend conda --conda-prefix .snakemake/conda \
           --configfile config.yaml \
-          --directory /home/sak0914/Mtb_Megapipe \
           --cores 8 --resources mem_mb=25000 \
           --rerun-incomplete --keep-going \
           --unlock
 
 
-snakemake --snakefile /home/sak0914/Mtb_Megapipe/snakefile \
-          --use-conda --conda-frontend conda --conda-prefix /home/sak0914/Mtb_Megapipe/.snakemake/conda \
+snakemake --snakefile snakefile \
+          --use-conda --conda-frontend conda --conda-prefix .snakemake/conda \
           --configfile config.yaml \
-          --directory /home/sak0914/Mtb_Megapipe \
           --cores 8 --resources mem_mb=25000 \
-          --rerun-incomplete --keep-going --allowed-rules pilon_get_variants_only_file exclude_low_confidence_sites combine_codon_variants annotate_variants_snpEff create_WHO_catalog_variants_TSV get_WHO_catalog_resistance_predictions #--dry-run
+          --rerun-incomplete --keep-going
